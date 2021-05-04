0380b2e83eefba0622374309604e48a70ef1116c
Øyvind Rønne <oyron@equinor.com>
2021-05-04 12:09:31 +0200

    Working React app skeleton

---
+++ src/client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Button, Typography } from '@equinor/eds-core-react'
+
+const App = () => (
+  <>
+    <Typography variant="h1" bold>Radix Test Application</Typography>
+
+  </>
+)
+
+export default App;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
-const auth = require('./auth/auth');
-const app = require('./app')(auth);
+const express = require('express');
+const expressApp = express();
+const path = require('path');
 const http = require('http');
+const httpLogger = require('./httpLogger');
 const logger = require('./logger');
 const port = process.env.PORT || 3100;
 
-const server = http.createServer(app);
+expressApp.use(express.static(path.join(__dirname, 'client', 'build')));
+expressApp.use(httpLogger);
+const server = http.createServer(expressApp);
 server.listen(port, () => logger.info(`Radix Test is running on ${port}`));
-
-module.exports = server;
